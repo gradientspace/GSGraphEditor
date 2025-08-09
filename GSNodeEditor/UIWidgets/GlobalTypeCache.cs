2d8f257077d3562753e4f2aedfcfd85d7475bd08
Ryan Schmidt <rms@rms80.com>
2025-08-08 23:12:12 -0400

    fix up copyrights

--- GSNodeEditor/UIWidgets/GlobalTypeCache.cs
+++ GSNodeEditor/UIWidgets/GlobalTypeCache.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.CodeAnalysis;
+// Copyright Gradientspace Corp. All Rights Reserved.
+using Microsoft.CodeAnalysis;
 using System;
 using System.Collections.Generic;
 using System.Linq;
