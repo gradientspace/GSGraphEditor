d3b628db8387d75cddd2de9f25ec1a0d4c53dc87
Ryan Schmidt <rms@rms80.com>
2025-12-15 22:25:06 -0500

    change namespace

--- NodeLibraries/GenAINodeLibrary/GenAINodeLibrary.cs
+++ NodeLibraries/GenAINodeLibrary/GenAINodeLibrary.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.AI;
 
 namespace Gradientspace.Nodes.GenAI
 {
-    [NodeFunctionLibrary("Gradientspace.GenAI")]
+    [NodeFunctionLibrary("GenerativeAI")]
     public static class GenAINodeLibrary
     {
         public static void Initialize()
